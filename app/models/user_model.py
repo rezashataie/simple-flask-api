ceb24ed4b6dff1e9b60eecca894917f74812fe3e
rezashataie <rezashataie75@gmail.com>
2024-11-10 00:00:04 +0330

    change User Model

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -4,19 +4,19 @@ from app import db
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)  # Auto-incrementing primary key
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    mobile = db.Column(db.String(15), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     name = db.Column(db.String(80), nullable=True, default=None)
     family = db.Column(db.String(80), nullable=True, default=None)
     email = db.Column(db.String(120), unique=True, nullable=True, default=None)
 
-    def __init__(self, username, password, name=None, family=None, email=None):
-        self.username = username
+    def __init__(self, mobile, password, name=None, family=None, email=None):
+        self.mobile = mobile
         self.password = password
         self.name = name
         self.family = family
         self.email = email
 
     def __repr__(self):
-        return f"<User {self.username}>"
+        return f"<User {self.mobile}>"
--- config.py
+++ config.py
@@ -5,11 +5,10 @@ class Config:
     DEBUG = True
 
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+        f"mysql+pymysql://{os.getenv('DB_USER', 'root')}:{os.getenv('DB_PASSWORD', '')}@{os.getenv('DB_HOST', 'localhost')}/{os.getenv('DB_NAME', 'flask')}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'u5sg3i37cegao8bptj9wd6ibezhdvvss6elzpitf2s6mzphi1ofg2f7s0ygv1kvm')
 
-    JWT_EXPIRATION_DELTA = int(os.getenv('JWT_EXPIRATION_DELTA'))
-
-    LOG_LEVEL = os.getenv('LOG_LEVEL')
+    JWT_EXPIRATION_DELTA = int(os.getenv('JWT_EXPIRATION_DELTA', 60))
+    LOG_LEVEL = os.getenv('LOG_LEVEL', 'DEBUG')
