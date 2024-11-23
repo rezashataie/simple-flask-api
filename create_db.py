8f24a2b22a0cd96d1163cd128ffaef6de6de3c58
rezashataie <rezashataie75@gmail.com>
2024-11-23 21:25:46 +0330

    rebuild tables

--- create_db.py
+++ create_db.py
@@ -1,7 +1,19 @@
 from app import create_app, db
+from app.models.category_model import Category
+from app.models.game_model import Game
+from app.models.user_model import User
+from app.models.word_model import Word
 
 app = create_app()
 
 with app.app_context():
-    db.create_all()
-    print("All tables created successfully!")
+    try:
+        print("Dropping all existing tables...")
+        db.drop_all()
+        print("Existing tables dropped successfully!")
+
+        print("Creating new tables...")
+        db.create_all()
+        print("All tables created successfully!")
+    except Exception as e:
+        print(f"An error occurred: {e}")
